f962c86d7c60d1c285787919678580b29911de36
olizilla <oli@zilla.org.uk>
2015-03-16 23:04:13 +0000

    Add groovy, rust

--- data/index.js
+++ data/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 
 module.exports = function (name) {
   return fs.createReadStream(pathTo(name, 'png'))
@@ -12,6 +13,15 @@ module.exports.url = function (name) {
   return require(pathTo(name, 'json')).source.url
 }
 
+module.exports.list = function () {
+  return fs.readdirSync(__dirname).filter(function (item) {
+    return ['.DS_Store', 'index.js'].indexOf(item) === -1
+  })
+}
+
 function pathTo(name, ext) {
-  return './' + name + '/' + name + '.' + ext
+  var dir = path.join(__dirname, name)
+  if (!ext) return dir
+  var file = name + '.' + ext
+  return path.join(dir, file)
 }
